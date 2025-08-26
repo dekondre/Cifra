2c4416de7a36f8f533fee49c492c9ae7c9e896a0
dekondre <dekondre@gmail.com>
2025-08-26 21:53:32 +0300

    у меня получилось. Натали - Молодец!
    перевод списка в .json

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,21 +7,41 @@
 //Сохраните JSON-массив в файл.
 
 
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        List<string> productList = new List<string>(3){"Чай-Утрин", "Кофе-Бодрин", "Какао-Позитивин"},
-        List<float> productPrice = new List<float>(3){55.5f, 222.22f, 111.11f},
-        List<string> productDescribe = new List<string>(3) { "чаёчек", "кофеёчек", "какао-какао-о-о-о" } ,
-        };  
+        List<Product> productList = new List<Product>()
+        {
+            new Product() { Name = "Чай-Утрин", Price = 55.5f, Description = "чаёчек" },
+            new Product() { Name = "Кофе-Бодрин", Price = 222.2f, Description = "кофеёчек" },
+            new Product() { Name = "Какао-Позитивин", Price = 111.1f, Description = "какао-какао-о-о-о" }
+        };
+
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+            NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString,
+            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+        string productsJson = JsonSerializer.Serialize(productList, options);
+        StreamWriter file = File.CreateText("productList.json");
+        file.WriteLine(productsJson);
+        file.Close();
     }
-}
-public class Product
-{
-    public string Name { get; set; }
-    public float Price { get; set; }
-    public string Description { get; set; }
+
+
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+    }
 }
