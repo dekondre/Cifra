b69c4a4cbdeada85d1321d100e74b1ae93c13b17
dekondre <dekondre@gmail.com>
2025-08-26 21:22:28 +0300

    inital commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задание 2: Сериализация коллекции объектов. 
+//Создайте класс Product с полями, представляющими информацию о 
+//продукте (название, цена, описание и т.д.). 
+//Создайте список, содержащий несколько объектов класса Product. 
+//Используйте System.Text.Json для сериализации списка продуктов в 
+//JSON-массив. 
+//Сохраните JSON-массив в файл.
+
+
+using System.Text.Json;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> productList = new List<string>(3){"Чай-Утрин", "Кофе-Бодрин", "Какао-Позитивин"},
+        List<float> productPrice = new List<float>(3){55.5f, 222.22f, 111.11f},
+        List<string> productDescribe = new List<string>(3) { "чаёчек", "кофеёчек", "какао-какао-о-о-о" } ,
+        };  
+    }
+}
+public class Product
+{
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public string Description { get; set; }
+}
---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задание 1: Сериализация объекта в JSON. 
+//Создайте класс Person с полями, представляющими информацию о 
+//человеке (имя, фамилия, возраст, адрес, e-mail, дата рождения). 
+//Создайте экземпляр этого класса, заполните его данными. 
+//Используйте библиотеку System.Text.Json для сериализации объекта Person 
+//в формат JSON. 
+//Сохраните JSON-строку в файл. 
+
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person()
+        {
+            Name = "Вася",
+            Surname = "Иванов",
+            Age = 20,
+            Mail = "vasya@mail.ru",
+            DateOfBirth = new DateTime(2000, 09, 01)
+        };
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+            NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString,
+            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+
+        string personJson = JsonSerializer.Serialize(person, options);
+        StreamWriter file = File.CreateText("person.json");
+        file.WriteLine(personJson);
+        file.Close();
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public string Mail { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
